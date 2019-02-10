476fa8dc5c6056d629298b760d76bfc47d6d7c86
John Berlin <n0tan3rd@gmail.com>
2019-02-10 01:32:33 -0500

    forgot the example file and updated readme text

--- example.js
+++ example.js
@@ -1,17 +1,3 @@
-chrome-remote-interface-extra
-=======================
-
-Like [fs-extra](https://github.com/jprichardson/node-fs-extra) but for the [chrome-remote-interface-extra](https://github.com/cyrus-and/chrome-remote-interface) by [cyrus-and](https://github.com/cyrus-and).
-
-The chrome-remote-interface-extra brings a [GoogleChrome/puppeteer](https://github.com/GoogleChrome/puppeteer) like api to the chrome-remote-interface as well as making many of the full CDP values puppetter hides available.
-
-Why? Simply put, puppetter's abstractions around the CDP are fantastic except for one thing its Googley (not easily extendable beyond their own vision for the project).
-
-For the CDP to take over and put both Selenium and WebDriver in the grave, not to mention make FireFox implement it (puuuuweeeeezzzz), we need to make using the CDP dead simple.
-
-#### More Documentation Forthcoming but heres an example
-
-```javascript
 const CDP = require('chrome-remote-interface')
 const { Browser, Page } = require('./lib')
 
@@ -58,4 +44,3 @@ async function koolKidsWay () {
   await puppeteeryWay()
   await koolKidsWay()
 })()
-```
