be4cce38435627254bfd50d82ea678c3b63e140b
John Berlin <n0tan3rd@gmail.com>
2019-02-18 14:07:02 -0500

    exposed more CDP values, Page, Emulation Manager

--- generateIndex.js
+++ generateIndex.js
@@ -1,34 +1,109 @@
+const CP = require('child_process')
 const Path = require('path')
 const fs = require('fs-extra')
 
 const libPath = Path.join(__dirname, 'lib')
-const stringSort = (s1, s2) => s1.localeCompare(s2)
+const stringSort = (s1, s2) => s1.trim().localeCompare(s2.trim())
+
+/**
+ * @param {string} filePath
+ * @return {string}
+ */
+const joinPathWithLibRoot = filePath => Path.join(libPath, filePath)
+
+/**
+ * @param {string} filePath
+ * @return {string}
+ */
+const filesBaseName = filePath => Path.basename(filePath, '.js')
+
+/**
+ * @param {string} filePath
+ * @return {string}
+ */
+const fileRequirePath = filePath => `./${filesBaseName(filePath)}`
+
+/**
+ * @param {string} filePath
+ * @return {Array<string>}
+ */
+function getFileExports (filePath) {
+  const moduleExports = []
+  let exportedKey
+  for (exportedKey in require(filePath)) {
+    moduleExports.push(exportedKey)
+  }
+  moduleExports.sort(stringSort)
+  return moduleExports
+}
+
+/**
+ * @param {string} folderPath
+ * @return {Array<string>}
+ */
+const getFolderExports = folderPath =>
+  getFileExports(Path.join(folderPath, 'index.js'))
+
+/**
+ * @param {string} filePath
+ * @return {Promise<boolean>}
+ */
+async function isPathToFile (filePath) {
+  const stat = await fs.stat(filePath)
+  return stat.isFile()
+}
+
+function formatGeneratedIndex () {
+  return new Promise((resolve, reject) => {
+    CP.exec(
+      `yarn run prettier-standard ${joinPathWithLibRoot('index.js')}`,
+      error => {
+        if (error) {
+          return reject(error)
+        }
+        resolve()
+      }
+    )
+  })
+}
 
 async function gen () {
   const libFiles = await fs.readdir(libPath)
+  const numFiles = libFiles.length
   const indexContents = []
   let indexExports = []
-  for (let i = 0; i < libFiles.length; i++) {
+  let i = 0
+  let file
+  let fileFullPath
+  let fileExports
+  for (; i < numFiles; i++) {
     if (libFiles[i] !== 'index.js') {
-      const file = libFiles[i]
-      const requirePath = `./${Path.basename(file, '.js')}`
+      file = libFiles[i]
       switch (file) {
         case 'USKeyboardLayout.js':
           indexContents.push(
-            `const USKeyboardLayout = require('${requirePath}')`
+            `const USKeyboardLayout = require('${fileRequirePath(file)}')`
           )
           indexExports.push('  USKeyboardLayout')
           break
         case 'chromeRemoteInterfaceExtra.js':
-          indexContents.push(`const CRIExtra = require('${requirePath}')`)
+          indexContents.push(
+            `const CRIExtra = require('${fileRequirePath(file)}')`
+          )
           indexExports.push('  CRIExtra')
           break
         default:
-          const exports = Object.keys(require(Path.join(libPath, file)))
-          indexExports = indexExports.concat(exports.map(e => `  ${e}`))
-          exports.sort(stringSort)
+          fileFullPath = joinPathWithLibRoot(file)
+          if (await isPathToFile(fileFullPath)) {
+            fileExports = getFileExports(joinPathWithLibRoot(file))
+          } else {
+            fileExports = getFolderExports(joinPathWithLibRoot(file))
+          }
+          indexExports.push(...fileExports)
           indexContents.push(
-            `const { ${exports.join(', ')} } = require('${requirePath}')`
+            `const { ${
+              fileExports.join(', ')
+            } } = require('${fileRequirePath(file)}')`
           )
           break
       }
@@ -41,6 +116,7 @@ async function gen () {
     indexContents.join('\n'),
     'utf8'
   )
+  await formatGeneratedIndex()
 }
 
 gen().catch(error => {
